faaf5fb116d203943e5daf6c90125ab3932e9416
101AoiKoori <1516396762@qq.com>
2024-06-07 01:40:32 +0800

    Snacks

--- Project/Cramer.c
+++ Project/Cramer.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include <stdlib.h>
+
 int matrix(int **arr, int size);
 
 void CramerRule(int **arr, int size1, int *answer, float *result);
@@ -34,13 +35,15 @@ int main()
     return 0;
 }
 
+//计算行列式
 int matrix(int **arr, int size)
 {
+    // 一阶行列式的处理
     if (size == 1)
     {
         return arr[0][0];
     }
-
+    // 开辟二维数组空间
     int **p = (int **)malloc(size * sizeof(int *));
 
     for (int i = 0; i < size; ++i)
@@ -50,9 +53,11 @@ int matrix(int **arr, int size)
 
     int i, j, k;
     int sum = 0;
+    // k用于计算代数余子式的正负号
     for (k = 0; k < size; ++k)
     {
         int m = 0;
+        // 以第一行展开计算
         for (i = 1; i < size; ++i)
         {
             int n = 0;
@@ -65,11 +70,12 @@ int matrix(int **arr, int size)
             }
             ++m;
         }
-
+        //判断正负号
         int sign = (k % 2 == 0) ? 1 : -1;
+        // 用递归计算行列式的值
         sum += sign * arr[0][k] * matrix(p, size - 1);
     }
-
+    //释放内存
     for (int i = 0; i < size; ++i)
     {
         free(p[i]);
@@ -79,25 +85,26 @@ int matrix(int **arr, int size)
     return sum;
 }
 
+//克莱姆法则计算线性方程组
 void CramerRule(int **arr, int size1, int *answer, float *result)
 {
     int det = matrix(arr, size1);
-
+    //判断特殊情况
     if (det == 0)
     {
         printf("行列式为空。\n");
         return;
     }
-
+    //开辟二维数组空间
     int **n = (int **)malloc(size1 * sizeof(int *)); // arr
     for (int i = 0; i < size1; ++i)
     {
         n[i] = (int *)malloc((size1 - 1) * sizeof(int));
     }
-
+    //a用于交替让answer在行列式的不同列切换
     for (int a = 0; a < size1; ++a)
     {
-
+        //复制数组
         for (int i = 0; i < size1; ++i)
         {
             for (int j = 0; j < size1; ++j)
@@ -105,16 +112,18 @@ void CramerRule(int **arr, int size1, int *answer, float *result)
                 n[i][j] = arr[i][j];
             }
         }
-
+        //交换列的位置
         for (int k = 0; k < size1; ++k)
         {
             n[k][a] = answer[k];
         }
-
+        //计算行列式
         int detAi = matrix(n, size1);
         printf("detAi=%d,det=%d\n", detAi, det);
+        //计算方程组的解（但不知道为什么输出的是整形）
         result[a] = detAi / det;
     }
+    //释放内存
     for (int i = 0; i < size1; ++i)
     {
         free(n[i]);
